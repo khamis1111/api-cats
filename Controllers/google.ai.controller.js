f928e25a05c174738d16061fd7bfb081d5ca754b
unknown <mahmood@gmail.com>
2024-11-13 22:44:25 +0200

    edit

--- Controllers/google.ai.controller.js
+++ Controllers/google.ai.controller.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const { GoogleAIFileManager } = require("@google/generative-ai/server");
-const fs = require("fs");
-const path = require("path");
+// const { GoogleAIFileManager } = require("@google/generative-ai/server");
+// const fs = require("fs");
+// const path = require("path");
 
 const aiConfigration = async (req, res, next) => {
   try {
@@ -29,7 +29,6 @@ const aiConfigration = async (req, res, next) => {
 const aiGemini = async (req, res) => {
   try {
     const { text } = req.body;
-    const file = req.file;
 
     const result = await req.model.generateContent({
       contents: [{ role: "user", parts: { text } }],
@@ -38,134 +37,134 @@ const aiGemini = async (req, res) => {
 
     return res
       .status(200)
-      .json({ status: "success", data: result.response.text(), file });
+      .json({ status: "success", data: result.response.text() });
   } catch (error) {
     return res.status(401).json({ status: "fail", err: error });
   }
 };
 
-const aiGeminiFile = async (req, res) => {
-  const { text } = req.body;
-  const file = req.file;
-  const fileManager = new GoogleAIFileManager(
-    "AIzaSyAw3fuaejoPbR6t3lEoI5TbxhOF1iVp_Gs"
-  );
-  let files;
+// const aiGeminiFile = async (req, res) => {
+//   const { text } = req.body;
+//   const file = req.file;
+//   const fileManager = new GoogleAIFileManager(
+//     "AIzaSyAw3fuaejoPbR6t3lEoI5TbxhOF1iVp_Gs"
+//   );
+//   let files;
 
-  if (file) {
-    // Function to upload file to Gemini
-    async function uploadToGemini(path, mimeType) {
-      try {
-        const uploadResult = await fileManager.uploadFile(path, {
-          mimeType,
-          displayName: path,
-        });
-        console.log(
-          `Uploaded file ${uploadResult.file.displayName} as: ${uploadResult.file.name}`
-        );
-        return uploadResult.file;
-      } catch (error) {
-        return res.status(401).json({
-          status: "fail",
-          err: `File upload failed: ${error.message}`,
-        });
-      }
-    }
+//   if (file) {
+//     // Function to upload file to Gemini
+//     async function uploadToGemini(path, mimeType) {
+//       try {
+//         const uploadResult = await fileManager.uploadFile(path, {
+//           mimeType,
+//           displayName: path,
+//         });
+//         console.log(
+//           `Uploaded file ${uploadResult.file.displayName} as: ${uploadResult.file.name}`
+//         );
+//         return uploadResult.file;
+//       } catch (error) {
+//         return res.status(401).json({
+//           status: "fail",
+//           err: `File upload failed: ${error.message}`,
+//         });
+//       }
+//     }
 
-    // Wait for files to be processed and active
-    async function waitForFilesActive(files) {
-      console.log("Waiting for file processing...");
-      for (const name of files.map((file) => file.name)) {
-        let file = await fileManager.getFile(name);
-        while (file.state === "PROCESSING") {
-          process.stdout.write(".");
-          await new Promise((resolve) => setTimeout(resolve, 10000));
-          file = await fileManager.getFile(name);
-        }
-        if (file.state !== "ACTIVE") {
-          return res.status(401).json({
-            status: "fail",
-            err: `File ${file.name} failed to process`,
-          });
-        }
-      }
-      console.log("...all files ready\n");
-    }
+//     // Wait for files to be processed and active
+//     async function waitForFilesActive(files) {
+//       console.log("Waiting for file processing...");
+//       for (const name of files.map((file) => file.name)) {
+//         let file = await fileManager.getFile(name);
+//         while (file.state === "PROCESSING") {
+//           process.stdout.write(".");
+//           await new Promise((resolve) => setTimeout(resolve, 10000));
+//           file = await fileManager.getFile(name);
+//         }
+//         if (file.state !== "ACTIVE") {
+//           return res.status(401).json({
+//             status: "fail",
+//             err: `File ${file.name} failed to process`,
+//           });
+//         }
+//       }
+//       console.log("...all files ready\n");
+//     }
 
-    // Upload the image file
-    const uploadedFile = await uploadToGemini(file.path, file.mimetype);
-    files = [uploadedFile];
+//     // Upload the image file
+//     const uploadedFile = await uploadToGemini(file.path, file.mimetype);
+//     files = [uploadedFile];
 
-    // Wait for the file to be ready for processing
-    await waitForFilesActive(files);
-  }
-  // Initiate the chat session with uploaded file and user text
-  const chatSession = req.model.startChat({
-    generationConfig: req.config,
-    history: [
-      req.file
-        ? {
-            role: "user",
-            parts: [
-              {
-                fileData: {
-                  mimeType: files[0].mimeType,
-                  fileUri: files[0].uri,
-                },
-              },
-              {
-                text:
-                  "read this file and choose the correct answer and return the correct answer only and choose one answer: " +
-                  text,
-              },
-            ],
-          }
-        : { role: "user", parts: [{ text }] },
-    ],
-  });
+//     // Wait for the file to be ready for processing
+//     await waitForFilesActive(files);
+//   }
+//   // Initiate the chat session with uploaded file and user text
+//   const chatSession = req.model.startChat({
+//     generationConfig: req.config,
+//     history: [
+//       req.file
+//         ? {
+//             role: "user",
+//             parts: [
+//               {
+//                 fileData: {
+//                   mimeType: files[0].mimeType,
+//                   fileUri: files[0].uri,
+//                 },
+//               },
+//               {
+//                 text:
+//                   "read this file and choose the correct answer and return the correct answer only and choose one answer: " +
+//                   text,
+//               },
+//             ],
+//           }
+//         : { role: "user", parts: [{ text }] },
+//     ],
+//   });
 
-  // Send the user-provided text to the chat session
-  const result = await chatSession.sendMessage(text);
-  console.log(result.response.text());
+//   // Send the user-provided text to the chat session
+//   const result = await chatSession.sendMessage(text);
+//   console.log(result.response.text());
 
-  const deleteFilesInUploads = () => {
-    const uploadsDir = path.resolve("uploads"); // Specify the uploads directory
+//   const deleteFilesInUploads = () => {
+//     const uploadsDir = path.resolve("uploads"); // Specify the uploads directory
 
-    // Read the files in the 'uploads' directory
-    fs.readdir(uploadsDir, (error, files) => {
-      if (error) {
-        return res.status(401).json({
-          status: "fail",
-          err: `Error reading the directory: ${error}`,
-        });
-      }
+//     // Read the files in the 'uploads' directory
+//     fs.readdir(uploadsDir, (error, files) => {
+//       if (error) {
+//         return res.status(401).json({
+//           status: "fail",
+//           err: `Error reading the directory: ${error}`,
+//         });
+//       }
 
-      // Loop through each file and delete it
-      files.forEach((file) => {
-        const filePath = path.join(uploadsDir, file);
+//       // Loop through each file and delete it
+//       files.forEach((file) => {
+//         const filePath = path.join(uploadsDir, file);
 
-        // Delete the file
-        fs.unlink(filePath, (err) => {
-          if (err) {
-            return res.status(401).json({
-              status: "fail",
-              err: `Error deleting file: ${filePath}, ${err}`,
-            });
-          } else {
-            console.log(`File deleted: ${filePath}`);
-          }
-        });
-      });
-    });
-  };
+//         // Delete the file
+//         fs.unlink(filePath, (err) => {
+//           if (err) {
+//             return res.status(401).json({
+//               status: "fail",
+//               err: `Error deleting file: ${filePath}, ${err}`,
+//             });
+//           } else {
+//             console.log(`File deleted: ${filePath}`);
+//           }
+//         });
+//       });
+//     });
+//   };
 
-  // Call the function to delete files
-  deleteFilesInUploads();
+//   // Call the function to delete files
+//   deleteFilesInUploads();
 
-  // Respond with AI response
-  return res
-    .status(200)
-    .json({ status: "success", data: result.response.text() });
-};
+//   // Respond with AI response
+//   return res
+//     .status(200)
+//     .json({ status: "success", data: result.response.text() });
+// };
 
-module.exports = { aiConfigration, aiGeminiFile };
+module.exports = { aiConfigration, aiGemini };
--- Routes/google.ai.route.js
+++ Routes/google.ai.route.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const {
   aiConfigration,
-  aiGeminiFile,
+  aiGemini,
 } = require("../Controllers/google.ai.controller");
-const { uploadFile } = require("../middleware/multer");
+// const { uploadFile } = require("../middleware/multer");
 
 const router = express.Router();
 
 router
   .route("/generate")
-  .post(aiConfigration, uploadFile.single("file"), aiGeminiFile);
+  // .post(aiConfigration, uploadFile.single("file"), aiGeminiFile);
+  .post(aiConfigration, uploadFile.single("file"), aiGemini);
 
 module.exports = router;

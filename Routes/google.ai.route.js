93c4a20a5ad9b0a64d1f476c95158c50e97d0050
unknown <mahmood@gmail.com>
2024-11-14 01:22:06 +0200

    add ai

--- Routes/google.ai.route.js
+++ Routes/google.ai.route.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const {
-  aiConfigration,
-  aiGemini,
-  aiGeminiFile,
+  textAndImg,
 } = require("../Controllers/google.ai.controller");
 const { uploadFile } = require("../middleware/multer");
 
 const router = express.Router();
 
+// router
+//   .route("/generate")
+//   .post(aiConfigration, uploadFile.single("file"), aiGeminiFile);
+
 router
   .route("/generate")
-  .post(aiConfigration, uploadFile.single("file"), aiGeminiFile);
-  // .post(aiConfigration, aiGemini);
+  .post(uploadFile.single("file"), textAndImg);
 
 module.exports = router;

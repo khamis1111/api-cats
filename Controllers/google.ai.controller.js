dd084d4ce3588032c36e8c7643b97298c9af17c9
unknown <mahmood@gmail.com>
2024-11-13 23:37:12 +0200

    edit

--- Controllers/google.ai.controller.js
+++ Controllers/google.ai.controller.js
@@ -8,11 +8,14 @@ const aiConfigration = async (req, res, next) => {
     const apiKey = "AIzaSyAw3fuaejoPbR6t3lEoI5TbxhOF1iVp_Gs";
     const genAI = new GoogleGenerativeAI(apiKey);
 
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-8b" });
+    const model = genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+    });
+
     const generationConfig = {
-      temperature: 2,
-      topP: 1,
-      topK: 40,
+      temperature: 1,
+      topP: 0.95,
+      topK: 64,
       maxOutputTokens: 8192,
       responseMimeType: "text/plain",
     };
@@ -27,20 +30,24 @@ const aiConfigration = async (req, res, next) => {
 };
 
 const aiGemini = async (req, res) => {
-  try {
-    const { text } = req.body;
+  //   try {
+  const { text } = req.body;
+  const result = await req.model.generateContent({
+    contents: [
+      {
+        role: "user",
+        parts: [{ text }],
+      },
+    ],
+    generationConfig: req.config,
+  });
 
-    const result = await req.model.generateContent({
-      contents: [{ role: "user", parts: { text } }],
-      generationConfig: req.config,
-    });
-
-    return res
-      .status(200)
-      .json({ status: "success", data: result.response.text() });
-  } catch (error) {
-    return res.status(401).json({ status: "fail", err: error });
-  }
+  return res
+    .status(200)
+    .json({ status: "success", data: result.response.text() });
+  //   } catch (error) {
+  //     return res.status(401).json({ status: "fail", err: error });
+  //   }
 };
 
 // const aiGeminiFile = async (req, res) => {
